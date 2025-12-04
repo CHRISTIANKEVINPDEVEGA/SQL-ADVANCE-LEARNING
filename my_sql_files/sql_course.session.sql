6229da15e4b9a95aa5382309f87e4707e8bf8932
christian kevin <christiandevega12@gmail.com>
2025-12-04 13:26:09 +0800

    first commit

---
+++ my_sql_files/sql_course.session.sql
@@ -0,0 +1,14 @@
+SELECT 
+    *
+FROM
+    job_postings_fact
+WHERE
+    salary_hour_avg = 70.0
+ORDER BY 
+    salary_year_avg DESC
+;
+
+SELECT 
+    job_posted_date::DATE
+FROM
+    job_postings_fact;
