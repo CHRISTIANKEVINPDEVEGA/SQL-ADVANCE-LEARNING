c1ce65a0269d2a9dcfe50afecba314d05cee5c7d
christian kevin <christiandevega12@gmail.com>
2025-12-04 15:39:51 +0800

    filter jobs for data analyst in philippines

--- project_sql/top_paying_job.sql
+++ project_sql/top_paying_job.sql
@@ -2,4 +2,9 @@ SELECT
     *
 FROM
     job_postings_fact
+WHERE
+    job_title LIKE '%Data%'
+    AND job_title LIKE '%Analyst%'
+    AND job_country = 'Philippines' 
+    AND salary_year_avg IS NOT NULL
 LIMIT 10;
