f933012643791879827ad956c4682a25afb63859
christian kevin <christiandevega12@gmail.com>
2025-12-04 16:58:24 +0800

    determine the demand count and the top 5 skills

--- project_sql/top_skills_data_analyst_jobs.sql
+++ project_sql/top_skills_data_analyst_jobs.sql
@@ -2,11 +2,23 @@
 Question: What are the top in demand skills for data analyst in 2023?
 */
 
+WITH data_analyst_job AS (
 SELECT 
-*
+    *
 FROM
-job_postings_fact
+    job_postings_fact
 WHERE
-job_title_short = 'Data Analyst'
-AND EXTRACT(YEAR FROM job_posted_date) = 2023
-LIMIT 10;
+    job_title_short = 'Data Analyst'
+    AND EXTRACT(YEAR FROM job_posted_date) = 2023
+)
+
+SELECT
+skills, type, COUNT(skills) AS demand_counts
+FROM 
+data_analyst_job 
+JOIN skills_job_dim ON skills_job_dim.job_id = data_analyst_job.job_id
+JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+GROUP BY skills, type
+ORDER BY COUNT(skills) DESC
+LIMIT 5
+;
